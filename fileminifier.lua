946fbbe6cff75b62fff5036cf40d786a51910a82
Rodrigo Régio de Araújo <boirodster9@gmail.com>
2022-06-28 20:16:30 -0300

    feat: Adicione funcionalidade para remoção de comentários baseados em java (/*,*/ e //).

--- fileminifier.lua
+++ fileminifier.lua
@@ -14,14 +14,14 @@ end
 
 function removeHtmlComments(textoDoArquivoMinificado)
 	local padrao = {"<!--", "%-%->"} --se a string desejada der errado tente colocando o caractere % na frente dos caracteres ignorados
-	local possuiComentarios=true
-	while possuiComentarios == true do
+	local possuiComentarios = true
+	while possuiComentarios do
 		local inicioComentario = {}
 		inicioComentario = string.find(textoDoArquivoMinificado, padrao[1])
 		local fimComentario = {}
 		fimComentario = string.find(textoDoArquivoMinificado, padrao[2])
 		if inicioComentario ~= nil and fimComentario ~= nil then
-			possuiComentarios=true
+			possuiComentarios = true
 		else
 			return textoDoArquivoMinificado
 		end
@@ -29,15 +29,50 @@ function removeHtmlComments(textoDoArquivoMinificado)
 		local teste, fimComentario = string.find(textoDoArquivoMinificado, padrao[2])    -- saber onde termina o comentário
 
 		local inicioTexto = string.sub(textoDoArquivoMinificado, 1, inicioComentario - 1)                            -- pega o inicio do texto até antes de iniciar o comentário
-		local fimTexto = string.sub(textoDoArquivoMinificado, fimComentario+1, string.len(textoDoArquivoMinificado)) -- pega o fim do texto de onde termina o comentário até o fim do código
+		local fimTexto = string.sub(textoDoArquivoMinificado, fimComentario + 1, string.len(textoDoArquivoMinificado)) -- pega o fim do texto de onde termina o comentário até o fim do código
 
 		textoDoArquivoMinificado = inicioTexto.. fimTexto
 	end
 end
 
-function removeJavaComments()
-	--[[ Todo: ]]
-	padrao = {'//', '/*', '*/'}
+function removeJavaMultipleLineComments(textoDoArquivoMinificado)
+	local padrao = {"/%*", "%*/"}
+	local possuiComentarios = true
+	while possuiComentarios do
+		local inicioComentario = {}
+		inicioComentario = string.find(textoDoArquivoMinificado, padrao[1])
+		local fimComentario = {}
+		fimComentario = string.find(textoDoArquivoMinificado, padrao[2])
+		if inicioComentario ~= nil and fimComentario ~= nil then
+			possuiComentarios = true
+		else
+			return textoDoArquivoMinificado
+		end
+		local inicioComentario, teste = string.find(textoDoArquivoMinificado, padrao[1])
+		local teste, fimComentario = string.find(textoDoArquivoMinificado, padrao[2])
+
+		local inicioTexto = string.sub(textoDoArquivoMinificado, 1, inicioComentario -1)
+		local fimTexto = string.sub(textoDoArquivoMinificado, fimComentario + 1, string.len(textoDoArquivoMinificado))
+
+		textoDoArquivoMinificado = inicioTexto.. fimTexto
+	end
+end
+
+function removeSingleLineComments(linha)
+	local padrao = {"//", "#"}
+	if string.find(linha, padrao[1]) ~= nil then
+		--testa //
+		local inicio, fim = string.find(linha, padrao[1])
+		linha = string.sub(linha, 1, inicio - 1)
+		return linha
+	elseif string.find(linha, padrao[2]) ~= nil then
+		--testa //
+		local inicio, fim = string.find(linha, padrao[2])
+		linha = string.sub(linha, 1, inicio - 1)
+		return linha
+	else
+		return linha
+	end
 end
 
 function minificaArquivo(arquivo)
@@ -46,12 +81,13 @@ function minificaArquivo(arquivo)
 		--remove \t (tabulações na linha)
 		linha = string.gsub(linha, "\t+","")
 		--[[ Colocar abaixo removedores de comentários que devem ser feitos durante a remoção de tabulações (\t) --]]
-		--
+		linha = removeSingleLineComments(linha)
 		--[[ Fim de removedores de comentários]]
 		primeiraLinha = primeiraLinha .. linha
 	end
 	--[[ Colocar abaixo removedores de comentários que possuem caracteres que iniciam e terminam de modos diferentes, como comentários de html e de java com multiplas linhas --]]
 	primeiraLinha = removeHtmlComments(primeiraLinha)
+	primeiraLinha = removeJavaMultipleLineComments(primeiraLinha)
 	--[[ fim de removedores de comentários]]
 	return primeiraLinha
 end
